6e1890a20031c2e5c2566fd6e727883e275122fc
jtonello <john@johntonello.com>
2020-02-03 15:52:05 -0500

    Cleaned up local IP bit.

--- start_dispynode.sh
+++ start_dispynode.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -e
 
-# Export all the container IP addresses to a file
-hostname -I > ip_addresses
-
 # Set the localIP variable to the first address in the ip_addresses list
-localIP=$(cat ip_addresses | awk '{print $1}')
+localIP=$(hostname -I > ip_addresses | awk '{print $1}')
 
 # Start dispy with the local (exposed) IP address
 dispynode.py -i $localIP -d --daemon
